69253262daf8b99ad1f57e7172c08654abaafdd8
hata0 <haataa0100@gmail.com>
2024-08-20 21:05:36 +0900

    feat: ConfigModule のインポートを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookModule } from './webhook/webhook.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [WebhookModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { WebhookService } from './webhook.service';
 import { WebhookController } from './webhook.controller';
-import { LineConfigService } from 'src/line-config/line-config.service';
 import { LineConfigService } from './line-config/line-config.service';
 
 @Module({
